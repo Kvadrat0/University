de083db9a0306bfe58c87457ec93777bf1c815d9
Kvadrat0 <144429626+Kvadrat0@users.noreply.github.com>
2023-11-09 05:07:29 +0300

    Delete PrimeNumber.py

--- PrimeNumber.py
+++
@@ -1,7 +0,0 @@
-prime = int(input('Enter a number: '))
-for i in range(2, prime):
-    if prime % i == 0:
-        print(prime, 'is not a prime number!')
-        break
-    if i == prime - 1:
-        print(prime, 'is a prime number!')
